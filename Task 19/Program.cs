00ea446b23e526162c684d0920fb00ef374629d3
Oleg Selivanov <alien3569@mail.ru>
2022-12-06 18:44:14 +0300

    Add Fin

--- Task 19/Program.cs
+++ Task 19/Program.cs
@@ -7,3 +7,20 @@
 */
 
 Console.Clear();
+
+Console.WriteLine("Введите пятизначное число: ");
+string? number = Console.ReadLine();
+
+void CheckingNumber(string number)
+    {
+        if (number[0]==number[4] || number[1]==number[3])
+            Console.WriteLine($"Ваше число: {number} - является палиндромом.");
+        else 
+            Console.WriteLine($"Ваше число: {number} - НЕ является палиндромом.");
+    }
+    {
+        if (number!.Length == 5)
+            CheckingNumber(number);
+        else 
+            Console.WriteLine($"Введенное число не пятизначное!");
+    }
